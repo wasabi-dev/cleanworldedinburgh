304d2a6406aa633e0753b4a83c4ee51b97cd4bb9
Cyntique <gcbtecno@gmail.com>
2020-05-30 12:43:36 -0300

    Cookies

--- js/script.js
+++ js/script.js
@@ -105,5 +105,24 @@ document.addEventListener('click', function chnageCount(evt) {
     }
 });
 
+function compruebaAceptaCookies() {
+    if(localStorage.aceptaCookies == 'true'){
+      cajacookies.style.display = 'none';
+    }
+  }
+  
+  /* aquí guardamos la variable de que se ha
+  aceptado el uso de cookies así no mostraremos
+  el mensaje de nuevo */
+  function aceptarCookies() {
+    localStorage.aceptaCookies = 'true';
+    cajacookies.style.display = 'none';
+  }
+  
+  /* ésto se ejecuta cuando la web está cargada */
+  $(document).ready(function () {
+    compruebaAceptaCookies();
+  });
+
 
     

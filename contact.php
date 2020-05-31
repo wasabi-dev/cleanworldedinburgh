5b942512c48d7425512ccdea76043c99309537dd
Daniel <danielmaio62@gmail.com>
2020-05-31 15:29:13 -0300

    cambios en form contact

--- contact.php
+++ contact.php
@@ -6,37 +6,37 @@
  * @see https://parzibyte.me/blog
  */
 
-if (empty($_POST["nombre"])) {
+if (empty($_POST["name"])) {
     exit("Please, write your name");
 }
 
-if (empty($_POST["correo"])) {
+if (empty($_POST["email"])) {
     exit("Please, write your email");
 }
 
-if (empty($_POST["mensaje"])) {
+if (empty($_POST["message"])) {
     exit("Please, write your message");
 }
 
-$nombre = $_POST["nombre"];
-$correo = $_POST["correo"];
-$mensaje = $_POST["mensaje"];
+$name = $_POST["name"];
+$email = $_POST["email"];
+$message = $_POST["message"];
 
-$correo = filter_var($correo, FILTER_VALIDATE_EMAIL);
-if (!$correo) {
+$email = filter_var($email, FILTER_VALIDATE_EMAIL);
+if (!$email) {
     echo "Wrong email";
     exit;
 }
 
 $asunto = "Nuevo mensaje de sitio web";
 
-$datos = "De: $nombre\nCorreo: $correo\nMensaje: $mensaje";
-$mensaje = "Has recibido un mensaje desde el formulario de contacto de tu sitio web. Aquí están los detalles:\n$datos";
+$datos = "De: $name\nCorreo: $email\nMensaje: $message";
+$message = "Has recibido un mensaje desde el formulario de contacto de tu sitio web. Aquí están los detalles:\n$datos";
 $destinatario = "Clean World Edinburgh"; # aquí la persona que recibirá los mensajes
 $encabezados = "Sender: cleanwor@cleanworldedinburgh.com\r\n"; # El remitente, debe ser un correo de tu dominio de servidor
-$encabezados .= "From: $nombre <" . $correo . ">\r\n";
-$encabezados .= "Reply-To: $nombre <$correo>\r\n";
-$resultado = mail($destinatario, $asunto, $mensaje, $encabezados);
+$encabezados .= "From: $name <" . $email . ">\r\n";
+$encabezados .= "Reply-To: $name <$email>\r\n";
+$resultado = mail($destinatario, $asunto, $message, $encabezados);
 if ($resultado) {
     echo "<h1>Mensaje enviado, ¡Gracias por contactarme!</h1>";
     echo "<p>Tu mensaje se ha enviado correctamente.</p><h2>Importante</h2><p>Revisa tu bandeja de SPAM, en ocasiones mis respuestas quedan ahí. </p>";

3221739d350aec6b724c7daa15189027094b9d78
Cyntique <gcbtecno@gmail.com>
2020-06-01 12:29:49 -0300

    Las funciones funcionan
    Pero tengo problemas para disparar la funcion

--- js/myscript.js
+++ js/myscript.js
@@ -1,5 +1,19 @@
-function type(){
-    document.getElementById('standard').value;
-}
+var phora= 17;
+var oven= 48;
+var dobleoven= 58;
+var carpet= 38;
+var window= 35;
 
 
+var canthoras = document.getElementById("canthoras").value;
+console.log(canthoras);
+
+
+document.getElementsById("signo").addEventListener("click", function preciototal(){
+    var h = canthoras * phora;
+    document.getElementById("mostrarhoras").innerHTML = h;     
+
+});
+
+console.log(preciototal);
+preciototal();

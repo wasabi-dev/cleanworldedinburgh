0300eef753f76581529434c5104351c75225f1e0
Cyntique <gcbtecno@gmail.com>
2020-06-30 14:48:02 -0300

    Prueba nav toggle

--- contact.php
+++ contact.php
@@ -33,7 +33,7 @@
         if ($success) {
             # Set a 200 (okay) response code.
             http_response_code(200);
-            echo "Thank you! Your message was sent correctly.";
+            echo "<script>alert('Thank you! Your message was sent correctly.');</script>";
         } else {
             # Set a 500 (internal server error) response code.
             http_response_code(500);

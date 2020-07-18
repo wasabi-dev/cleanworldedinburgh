a710b61acd17406e8b815fdb0b1cbd5e5d0875a4
Cynty <gcbtecno@gmail.com>
2020-07-17 21:11:56 -0300

    Pruebas3

--- formcod/formulario.js
+++ formcod/formulario.js
@@ -25,7 +25,7 @@ export default class Frame {
     form.append('street',this.data.street);
     form.append('typeClean',this.data.typeClean);
       
-    fetch('email.php',{
+    fetch('formcod/email.php',{
       method:'POST',
       body:form
   }).then(res=>{
